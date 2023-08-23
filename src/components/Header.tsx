6517c63d1e08b7c31b576357ccf6afc4eb28d120
DenisKibe <kibedenis@gmail.com>
2023-08-23 11:32:01 +0300

    UI update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,15 @@
 import { GiStoneWheel } from 'react-icons/gi'
+import { FaBars } from 'react-icons/fa'
+import NavBar from './Navigation'
+import { useState } from 'react';
 
 const Header = () =>{
+    const [showNav, setShowNav] = useState(false);
     return(
         <header>
             <GiStoneWheel className='logo'/>
-            <nav>
-                <ul>
-                    <li><a href='/'>Home</a></li>
-                    <li><a href='/reviews'>Reviews</a></li>
-                    <li><a href='/news'>News</a></li>
-                    <li><a href='/articles'>Articles</a></li>
-                </ul>
-            </nav>
+            {showNav ? <NavBar/> : ''}
+            <FaBars className='fa-bars' onClick={()=>setShowNav(!showNav)}/>
         </header>
     )
 }
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,14 @@
+const NavBar = () =>{
+    return(
+            <nav>
+                <ul>
+                    <li><a href='/'>Home</a></li>
+                    <li><a href='/reviews'>Reviews</a></li>
+                    <li><a href='/news'>News</a></li>
+                    <li><a href='/articles'>Articles</a></li>
+                </ul>
+            </nav>
+    )
+}
+
+export default NavBar;
